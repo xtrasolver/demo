9d1a1e48d0ccd74b163f2d0a2c7aadf24fb7c18c
Daishan Peng <daishan@rancher.com>
2019-02-12 10:19:02 -0700

    update

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hi there, i am StrongMonkey")
+	fmt.Fprintln(w, "Hi there, I am StrongMonkey:v1")
 }
 
 func main() {
