895a48b1f6fe2bd492e1246b6b977518ff5f1e12
Daishan Peng <daishan@rancher.com>
2019-05-20 14:02:37 -0700

    foo

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I am StrongMonkey:v26")
+	fmt.Fprintln(w, "Hi there, I am StrongMonkey:v27")
 }
 
 func main() {
