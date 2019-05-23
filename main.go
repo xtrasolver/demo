de37e5041cf3b2e935aa613861555dab491c8ed4
xtrasolver <43985181+xtrasolver@users.noreply.github.com>
2019-05-23 16:48:18 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I am mole:v3")
+	fmt.Fprintln(w, "Hi there, I am mole:v4")
 }
 
 func main() {
